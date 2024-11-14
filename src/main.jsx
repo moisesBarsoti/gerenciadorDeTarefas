9c404c44d69a66793027bbf4b0875c22fff5efcd
moisesBarsoti <moises.barsot2@gmail.com>
2024-11-14 18:20:34 -0300

    Subindo Arquivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { TaskPage } from './pages/TaskPage.jsx';
-import './index.css'
-
+import { TaskPage } from "./pages/TaskPage.jsx";
+import { NotFoundPage } from "./pages/NotFoundPage.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/gerenciadordetarefas",
     element: <App />,
   },
   {
     path: "/task",
     element: <TaskPage />,
   },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
